80a26264951d1127308681813a32b35d3323013e
MajdKhalifa <majdkhalifa321@gmail.com>
2026-01-28 20:15:01 +0300

    before merge

--- radiology_center_app/lib/main.dart
+++ radiology_center_app/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
             ),
           ),
 
-          initialRoute: AppRoute.aiAnalysis,
+          initialRoute: AppRoute.login,
         );
       },
       child: PatientDetails(),
